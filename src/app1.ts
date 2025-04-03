5b47ebeccfcbc866eb67d5962798f462a92d6b86
Larry Leung <lleung@virbela.com>
2025-04-03 10:54:14 -0400

    * established vite project and babylonjs * setup the app.ts bjs scene * added the app.ts 	* figured out how to load a glb file
     On branch master
    
     Initial commit
    
     Changes to be committed:
    	new file:   index.html
    	new file:   public/monkey.glb
    	new file:   src/app.ts
    	new file:   src/app1.ts
    
     Untracked files:
    	.gitignore
    	LICENSE
    	README.md
    	package-lock.json
    	package.json
    	src/counter.ts
    	src/main.ts
    	src/style.css
    	src/typescript.svg
    	src/vite-env.d.ts
    	tsconfig.json

---
+++ src/app1.ts
@@ -0,0 +1,52 @@
+import "@babylonjs/core/Debug/debugLayer";
+import "@babylonjs/inspector";
+
+import { SceneLoader } from '@babylonjs/core/Loading/sceneLoader';
+import '@babylonjs/loaders/glTF';
+
+import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
+
+class App {
+    constructor() {
+        // create the canvas html element and attach it to the webpage
+        var canvas = document.createElement("canvas");
+        canvas.style.width = "100%";
+        canvas.style.height = "100%";
+        canvas.id = "gameCanvas";
+        document.body.appendChild(canvas);
+
+        // initialize babylon scene and engine
+        var engine = new Engine(canvas, true);
+        var scene = new Scene(engine)
+
+        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 3, Vector3.Zero(), scene);
+        camera.attachControl(canvas, true);
+        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+
+
+        SceneLoader.ImportMesh("Suzanne", "./", "monkey.glb", scene, function(newMeshes){
+            const monkey = newMeshes[0];
+            monkey.scaling = new Vector3(0.5, 0.5, 0.5); // Scale it down to reasonable size
+            monkey.position = new Vector3(0, 0, 0); // Position at center
+        });
+
+
+        // hide/show the Inspector
+        window.addEventListener("keydown", (ev) => {
+            // Shift+Ctrl+Alt+I
+            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
+                if (scene.debugLayer.isVisible()) {
+                    scene.debugLayer.hide();
+                } else {
+                    scene.debugLayer.show();
+                }
+            }
+        });
+
+        // run the main render loop
+        engine.runRenderLoop(() => {
+            scene.render();
+        });
+    }
+}
+new App();
