54059c554a32577466d05ef1eade6cb4d3d9d6be
Larry Leung <lleung@virbela.com>
2025-04-29 02:43:27 -0400

    * file modified:   app7.ts         * vimdiff the changes from 04282025-llgc-bjs-enhancement             * brought over the AxesViewer             * the inspector embed feature             * the scene color     * reduced the AxesViewer size to 0.5     * imported the bjs gridmaterial module and apply grid material to the ground mesh     * added meshDebugPluginMaterial and enabled wireframe display on meshes     * created and adjusted the sofa material
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   app7.ts
    
     Changes not staged for commit:
    	modified:   app.ts
    
     Untracked files:
    	../.gitignore
    	../LICENSE
    	../package-lock.json
    	../package.json
    	app7.ts.orig
    	counter.ts
    	main.ts
    	style.css
    	typescript.svg
    	vite-env.d.ts
    	../tsconfig.json

--- src/app7.ts
+++ src/app7.ts
@@ -1,6 +1,7 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
-import { Engine, Scene, ArcRotateCamera, Color4, Vector4, Vector3, HemisphericLight, Mesh, MeshBuilder, VertexBuffer, MeshDebugPluginMaterial } from "@babylonjs/core";
+import { GridMaterial } from '@babylonjs/materials/Grid';
+import { Engine, Scene, AxesViewer, ArcRotateCamera, Color4, Color3, Vector4, Vector3, HemisphericLight, Mesh, MeshBuilder, StandardMaterial, VertexBuffer, MeshDebugPluginMaterial, MeshDebugMode } from "@babylonjs/core";
 
 class App {
     constructor() {
@@ -14,6 +15,7 @@ class App {
         // initialize babylon scene and engine
         var engine = new Engine(canvas, true);
         var scene = new Scene(engine);
+	scene.clearColor = new Color3( .4, .5, .5);
 
         // Create camera with position (1, 1, 0) and target at origin
         var camera: ArcRotateCamera = new ArcRotateCamera("Camera", 0, Math.PI / 2, 3, Vector3.Zero(), scene);
@@ -23,26 +25,31 @@ class App {
         camera.attachControl(canvas, true);
         var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
 
+	var defaultGridMaterial = new GridMaterial("default", scene);
+	defaultGridMaterial.majorUnitFrequency = 5;
+	defaultGridMaterial.gridRatio = 0.5;
+
         const c_ground = MeshBuilder.CreateGround("ground", {width: 2, height: 2}, scene);
+	c_ground.material = defaultGridMaterial;
 
-        const c_box = MeshBuilder.CreateBox("Box", {size: 0.1}, scene)
+//      const c_box = MeshBuilder.CreateBox("Box", {size: 0.1}, scene)
 
-        var box_colors = c_box.getVerticesData(VertexBuffer.ColorKind);
-        if(!box_colors) {
-            // Get the number of vertices
-            let positions = c_box.getVerticesData(VertexBuffer.PositionKind);
-            let vertexCount = positions ? positions.length / 3 : 0;
-            
-            // Create a Float32Array with red color (1,0,0,1) for each vertex
-            box_colors = new Float32Array(vertexCount * 4);
-            for(let i = 0; i < vertexCount; i++) {
-                box_colors[i * 4] = 0;     // R
-                box_colors[i * 4 + 1] = 1; // G
-                box_colors[i * 4 + 2] = 0; // B
-                box_colors[i * 4 + 3] = 1; // A
-            }
-        }
-        c_box.setVerticesData(VertexBuffer.ColorKind, box_colors);
+//      var box_colors = c_box.getVerticesData(VertexBuffer.ColorKind);
+//      if(!box_colors) {
+//          // Get the number of vertices
+//          let positions = c_box.getVerticesData(VertexBuffer.PositionKind);
+//          let vertexCount = positions ? positions.length / 3 : 0;
+//          
+//          // Create a Float32Array with red color (1,0,0,1) for each vertex
+//          box_colors = new Float32Array(vertexCount * 4);
+//          for(let i = 0; i < vertexCount; i++) {
+//              box_colors[i * 4] = 0;     // R
+//              box_colors[i * 4 + 1] = 1; // G
+//              box_colors[i * 4 + 2] = 0; // B
+//              box_colors[i * 4 + 3] = 1; // A
+//          }
+//      }
+//      c_box.setVerticesData(VertexBuffer.ColorKind, box_colors);
 
         //Shape profile in XY plane with offset
         const offset = new Vector3(-0.5, -0.5, 0);
@@ -63,50 +70,60 @@ class App {
             new Vector3(3, 0, 3),
         ];
 
-        const extrusion = MeshBuilder.ExtrudeShape("chair", {shape: myShape, closeShape: true, path: myPath, cap: Mesh.CAP_ALL, sideOrientation: Mesh.DOUBLESIDE}, scene);		
+        const extrusion = MeshBuilder.ExtrudeShape("sofa", {shape: myShape, closeShape: true, path: myPath, cap: Mesh.CAP_ALL, sideOrientation: Mesh.DOUBLESIDE}, scene);		
 
         extrusion.scaling = new Vector3(0.25, 0.25, 0.25);
         extrusion.position = new Vector3(0.125, 0.125, 0);
 
-        //If no colors add colors to sphere
-        var ext_colors = extrusion.getVerticesData(VertexBuffer.ColorKind);
-        if(!ext_colors) {
-            // Get the number of vertices
-            const positions = extrusion.getVerticesData(VertexBuffer.PositionKind);
-            const vertexCount = positions ? positions.length / 3 : 0;
-            
-            // Create a Float32Array with red color (1,0,0,1) for each vertex
-            ext_colors = new Float32Array(vertexCount * 4);
-            for(let i = 0; i < vertexCount; i++) {
-                ext_colors[i * 4] = 1;     // R
-                ext_colors[i * 4 + 1] = 0; // G
-                ext_colors[i * 4 + 2] = 0; // B
-                ext_colors[i * 4 + 3] = 1; // A
-            }
+//      //If no colors add colors to sphere
+//      var ext_colors = extrusion.getVerticesData(VertexBuffer.ColorKind);
+//      if(!ext_colors) {
+//          // Get the number of vertices
+//          const positions = extrusion.getVerticesData(VertexBuffer.PositionKind);
+//          const vertexCount = positions ? positions.length / 3 : 0;
+//          
+//          // Create a Float32Array with red color (1,0,0,1) for each vertex
+//          ext_colors = new Float32Array(vertexCount * 4);
+//          for(let i = 0; i < vertexCount; i++) {
+//              ext_colors[i * 4] = 1;     // R
+//              ext_colors[i * 4 + 1] = 0; // G
+//              ext_colors[i * 4 + 2] = 0; // B
+//              ext_colors[i * 4 + 3] = 1; // A
+//          }
+//      }
+
+//      extrusion.setVerticesData(VertexBuffer.ColorKind, ext_colors); 
+
+// show debug info:
+        extrusion.showBoundingBox = true;
+
+	// Create a material
+        const sofaMaterial = new StandardMaterial("sofaMaterial", scene);
+        sofaMaterial.diffuseColor = new Color3(0.8, 0.5, 0.5);
+        sofaMaterial.specularColor = new Color3(0.1, 0.1, 0.1);
+//      sofaMaterial.wireframe = true; // Enable wireframe mode
+        extrusion.material = sofaMaterial;
+
+// Enable wireframe debug display on all meshes
+        // Modify mesh's geometry to prepare for TRIANGLES mode in plugin
+        for (const mesh of scene.meshes) {
+		MeshDebugPluginMaterial.PrepareMeshForTrianglesAndVerticesMode(mesh);
         }
 
-        extrusion.setVerticesData(VertexBuffer.ColorKind, ext_colors); 
-
-//      extrusion.showSubMeshes = true;
-//      extrusion.enableEdgesRendering();
-//      extrusion.edgesWidth = 1;
-//      extrusion.edgesColor = new Color4(0, 1, 0, 1);
-
-
-    // Modify mesh's geometry to prepare for TRIANGLES mode in plugin
-    for (const mesh of scene.meshes) {
-       MeshDebugPluginMaterial.PrepareMeshForTrianglesAndVerticesMode(mesh);
-    }
-
-    // Add plugin to all materials
-    for (const material of scene.materials) {
-        const plugin = new MeshDebugPluginMaterial(material, {
-            mode: MeshDebugMode.TRIANGLES,
-        });
-    }
+        // Add plugin to all materials
+        for (const material of scene.materials) {
+            const plugin = new MeshDebugPluginMaterial(material, {
+                mode: MeshDebugMode.TRIANGLES, wireframeTrianglesColor: new Color3(0, 0.5, 0),
+            });
+        }
 
 
-
+// create axes display after the wireframe material
+	// https://doc.babylonjs.com/toolsAndResources/utilities/World_Axes/
+	// https://doc.babylonjs.com/typedoc/classes/BABYLON.Debug.AxesViewer
+	// https://playground.babylonjs.com/#T8UQTA
+	//new AxesViewer(scene, 0.5);
+	const c_Axes = new AxesViewer(scene, 0.5);
 
 
         // hide/show the Inspector
@@ -116,7 +133,12 @@ class App {
                 if (scene.debugLayer.isVisible()) {
                     scene.debugLayer.hide();
                 } else {
-                    scene.debugLayer.show();
+                  //scene.debugLayer.show();
+                    // combine the inspector with the outliner: https://forum.babylonjs.com/t/show-scene-explorer-on-right-side-without-inspection/13719 
+		    // api: https://doc.babylonjs.com/typedoc/interfaces/BABYLON.IInspectorOptions
+		    scene.debugLayer.show({
+			embedMode:true
+		    });
                 }
             }
         });
