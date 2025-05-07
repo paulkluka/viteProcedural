09ab597f335f44fef686edf2293a857eeb2c6898
Larry Leung <lleung@virbela.com>
2025-05-07 02:20:23 -0400

    * modified:   src/assets/abstract01.ts     * added the correct faceUV to the box (base)     * added the noiseProceduralTexture     * adjusted the procedural texture parameters
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   src/assets/abstract01.ts
    
     Changes not staged for commit:
    	modified:   src/app.ts
    
     Untracked files:
    	.gitignore
    	LICENSE
    	package-lock.json
    	package.json
    	src/counter.ts
    	src/main.ts
    	src/style.css
    	src/typescript.svg
    	src/vite-env.d.ts
    	tsconfig.json

--- src/assets/abstract01.ts
+++ src/assets/abstract01.ts
@@ -1,6 +1,6 @@
 // create an abstract prop
-import { Scene, MeshBuilder, Mesh, Color3, Vector3, Curve3, StandardMaterial, Texture } from "@babylonjs/core";
-
+import { Scene, MeshBuilder, Mesh, Vector4, Color3, Vector3, Curve3, StandardMaterial, Texture, NoiseProceduralTexture} from "@babylonjs/core";
+import { MarbleProceduralTexture } from "@babylonjs/procedural-textures/marble";
 // create the class
 export class Abstract01 {
 	// create the method
@@ -9,8 +9,18 @@ export class Abstract01 {
         	const c_baseSize = 0.25;
         	const c_baseHeight = 0.5;
         	const c_baseGroundSnap = (c_baseSize * 0.5) * c_baseHeight;
+
+		// map the uv of each faces of the box
+		const c_faceUV = [];
+		c_faceUV[0] = new Vector4(0.0, 0.0, 1.0, c_baseHeight); //rear face
+		c_faceUV[1] = new Vector4(0.0, 0.0, 1.0, c_baseHeight); //front face
+		c_faceUV[2] = new Vector4(0.0, 0.0, 1.0, c_baseHeight); //right side
+		c_faceUV[3] = new Vector4(0.0, 0.0, 1.0, c_baseHeight); //left side
+		c_faceUV[4] = new Vector4(0.0, 0.0, 1.0, 1.0); //top side
+		c_faceUV[5] = new Vector4(0.0, 0.0, 1.0, 1.0); //bottom side
+
         	// base mesh creation using the parameters
-        	const c_base = MeshBuilder.CreateBox("base", { size: c_baseSize }, scene);
+        	const c_base = MeshBuilder.CreateBox("base", { faceUV: c_faceUV, size: c_baseSize }, scene);
         	c_base.scaling = new Vector3(1, c_baseHeight, 1);
         	c_base.position = new Vector3(0, c_baseGroundSnap, 0);
         	console.log(c_baseGroundSnap);
@@ -72,10 +82,26 @@ export class Abstract01 {
 		const c_mesh = Mesh.MergeMeshes([c_base, c_extruded])
 		c_mesh.bakeCurrentTransformIntoVertices()//reset the transform
 
+		// set material
 		const c_mesh_mat = new StandardMaterial("c_mesh_mat", scene)
 		c_mesh_mat.diffuseTexture = new Texture("https://www.babylonjs-playground.com/textures/lava/lavatile.jpg");
-		c_mesh_mat.diffuseColor = new Color3(0.5, 0.7, 0.7)
-		c_mesh.material = c_mesh_mat
+		// procedural textures
+        	const c_procTxr = new NoiseProceduralTexture("perlin", 256, scene);
+        	c_procTxr.octaves = 5
+        	c_procTxr.level = 0.21
+        	c_procTxr.persistence = 1.3
+        	c_procTxr.animationSpeedFactor = 0
+//      	const c_procTxr = new MarbleProceduralTexture("marble", 256, scene);
+//      	c_procTxr.numberOfTilesHeight = 3
+//      	c_procTxr.numberOfTilesWidth = 3
+//      	c_procTxr.amplitude = 9.0
+//      	c_procTxr.jointColor = new Color3(0.72, 0.72, 0.72)
+        	c_mesh_mat.diffuseTexture = c_procTxr
+
+
+        	c_mesh_mat.diffuseColor = new Color3(0.5, 0.7, 0.7)
+        	c_mesh_mat.specularPower = 6
+        	c_mesh.material = c_mesh_mat
 		
         	return c_mesh; // for the purpose of using a variable to capture the output of this class
 	}
